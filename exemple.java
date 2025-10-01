69ae94070c139f7dde9a53953701aa9031c4d9b7
Sebastian <jsorio19950@iesjoanramis.org>
2025-10-01 19:28:46 +0200

    Programa suma

--- exemple.java
+++ exemple.java
@@ -3,10 +3,16 @@
 /*Esto es un comentario de mas de una linea Ex3.3 en la rama comentaris
 Esto es otro comentario de mas de una linea en el archivo exemple.java*/
 
+//Programa que suma dos valores y lo retorna en un resultado
 public class exemple{
 	public static void main(String[]args){
-		a-b;
-//Comentari en català
+	int num1;
+	int num2;
+	int resultado;
+	
+	resultado = num1+num2;
+
+	System.out.println("La suma de ambos numeros es: "+resultado);
 	}
 	
 }
