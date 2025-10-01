f293694e1d397d9eb4eb9abb0d92362876743a5c
Sebastian <jsorio19950@iesjoanramis.org>
2025-10-01 18:52:23 +0200

    Añadido de comentarios

--- exemple.java
+++ exemple.java
@@ -1,9 +1,12 @@
 //Comentari programa github
 //segundo comentario
+/*Esto es un comentario de mas de una linea Ex3.3 en la rama comentaris
+Esto es otro comentario de mas de una linea en el archivo exemple.java*/
 
 public class exemple{
 	public static void main(String[]args){
 		a-b;
+//Comentari en català
 	}
 	
 }
